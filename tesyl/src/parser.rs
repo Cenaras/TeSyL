6eee0ac682ae58e380849011e096fcf4dff80539
Mathias <mathias_rud@hotmail.com>
2022-04-29 14:37:10 +0200

    new branch to rework parser

--- tesyl/src/parser.rs
+++ tesyl/src/parser.rs
@@ -14,6 +14,9 @@ type ErrorType = &'static str;
 // Implement "eat" function that throws error if ate token is not expected.
 
 
+// Useful reference implementation might be: 
+// https://github.com/antoyo/tiger-rs/blob/master/tiger/src/parser.rs
+
 pub struct Parser {
     tokens: TokenIter,
 }
@@ -135,51 +138,6 @@ impl Parser {
 
 
     // ##### TESTING STUFF #####
-
-    /* JAVA PARSER WORK
-        //https://en.wikipedia.org/wiki/Operator-precedence_parser
-
-        private Exp parseExpLeftToRight(Exp base, int minPrecedence) throws IOException {
-            TokenType lookahead = nextToken.getType();
-            while (precedence.getOrDefault(lookahead, -1) >= minPrecedence ) {
-                TokenType op = nextToken.getType();
-                eat(op);
-                Exp right = parseBaseExp();
-                lookahead = nextToken.getType();
-                while (precedence.getOrDefault(lookahead, -1) > precedence.get(op)) { //If right associative then >= else just >: When adding ^ to language, get a "isRightAssoc()" and make a || case with >=
-                     right = parseExpLeftToRight(right, precedence.get(op) + 1);
-                     lookahead = nextToken.getType();
-                }
-                base = new BinOpExp(base, op, right);
-            }
-            return base;
-        }
-
-
-    */
 }
 
-/*
-#[macro_export]
-macro_rules! peek_op_or_err {
-    ($self:ident) => {
-        match $self.tokens.peek() {
-            Some(Token::PLUS) => BinOp::PlusBinOp,
-            _ => return Err("Expected a symbol"),
-        }
-    };
-}
-use crate::peek_literal_or_err;
-
-#[macro_export]
-macro_rules! peek_literal_or_err {
-    ($self:ident) => {
-        match $self.tokens.peek() {
-            Some(Token::IntLit(value)) => Exp::IntExp(value.clone()),
-            _ => return Err("Expected a literal"),
-        }
-    };
-}
-
-*/
 
